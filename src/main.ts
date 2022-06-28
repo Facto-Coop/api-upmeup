8e335b6ecd7431c9d48fdb46b56f448bbb2147b9
LMata-Facto <lmata@facto.cat>
2022-06-28 17:32:19 +0200

    added EnableCors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  //app.enableCors();
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(3000);
 
   Logger.log('', 'Start');
   Logger.log('API RUNNING: http://localhost', 'Start');
   Logger.log('', 'Start');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
