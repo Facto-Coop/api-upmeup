aa6a1feeaf166c5bd36e9f8ee990d517abb96797
LMata-Facto <lmata@facto.cat>
2022-08-02 12:50:11 +0200

    Added global constant DEF_PORT

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
+/* eslint-disable prettier/prettier */
 export const MONO_DB_CONNECTION_STRING =
 	process.env.MONO_DB_CONNECTION_STRING || 'mongodb://localhost:27017/UMU_dbApp';
+
+
+export const DEF_PORT = 3000;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DEF_PORT } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,7 +14,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(DEF_PORT, '0.0.0.0');
 
   Logger.log('', 'Start');
   Logger.log(`API RUNNING: ${await app.getUrl()}`, 'Start');
